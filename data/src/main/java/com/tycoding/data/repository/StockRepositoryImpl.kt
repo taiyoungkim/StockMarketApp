1c866bcc18847b2b083596f8c3c2376e3ed3d190
taiyoungkim <ktyoung153@gmail.com>
2022-10-03 16:38:39 +0900

    get company list data repository

---
+++ data/src/main/java/com/tycoding/data/repository/StockRepositoryImpl.kt
@@ -0,0 +1,54 @@
+package com.tycoding.data.repository
+
+import com.opencsv.CSVReader
+import com.tycoding.data.local.StockDatabase
+import com.tycoding.data.mapper.toCompanyListing
+import com.tycoding.data.remote.StockApi
+import com.tycoding.domain.model.CompanyListing
+import com.tycoding.domain.repository.StockRepository
+import com.tycoding.domain.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import java.io.InputStreamReader
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class StockRepositoryImpl @Inject constructor(
+    val api: StockApi,
+    val db: StockDatabase
+): StockRepository {
+
+    private val dao = db.dao
+
+    override suspend fun getCompanyListings(
+        fetchFromRemote: Boolean,
+        query: String
+    ): Flow<Resource<List<CompanyListing>>> {
+        return flow {
+            emit(Resource.Loading(true))
+            val localListings = dao.searchCompanyListing(query)
+            emit(Resource.Success(
+                data = localListings.map { it.toCompanyListing() }
+            ))
+
+            val isDbEmptry = localListings.isEmpty() && query.isBlank()
+            val shouldJustLoadFromCache = !isDbEmptry && !fetchFromRemote
+            if (shouldJustLoadFromCache) {
+                emit(Resource.Loading(false))
+                return@flow
+            }
+            val remoteListings = try {
+                val response = api.getListings()
+            } catch (e: IOException) {
+                e.printStackTrace()
+                emit(Resource.Error("Couldn't load data"))
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emit(Resource.Error("Couldn't load data"))
+            }
+        }
+    }
+}
